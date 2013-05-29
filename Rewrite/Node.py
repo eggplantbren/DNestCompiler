c59a5d6d586322b3a5021cfb85677b48f7b38871
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-05-29 19:16:00 +1200

    Changes

--- Rewrite/Node.py
+++ Rewrite/Node.py
@@ -1,37 +1,90 @@
-class Node:
-	def __init__(self, name, distribution, observed=None):
-		"""
-		Inputs are the name of the variable
-		and its prior distribution which may
-		depend on other variables. Also, the
-		observed value, which defaults to `None`.
-		"""
+class Uniform:
+	def __init__(self, name, a, b):
 		self.name = name
-		self.distribution = distribution
-		self.observed = None
+		self.a = a
+		self.b = b
 
-	def from_prior(self):
-		return self.distribution.from_prior(self.name)
+	def __repr__(self):
+		return '{name} ~ uniform({a}, {b});'.format(name=self.name,
+						a=self.a, b=self.b)
 
-	def proposal(self):
-		return self.distribution.proposal(self.name)
-
-	def log_likelihood(self):
-		return self.distribution.log_density(self.name).replace('logH',
-							'logL')
 	def __str__(self):
 		return self.name
 
-if __name__ == '__main__':
-	from Distribution import Uniform
+	def from_prior(self):
+		s = ''
+		s += '{name} = {a} + ({b} - {a})*randomU();\n'
+		return s.format(name=self.name, a=self.a, b=self.b)		
 
-	n1 = Node(name='theta', distribution=Uniform(0., 1.))
-	n2 = Node(name='x', distribution=Uniform(0., n1))
+	def log_likelihood(self):
+		s = ''
+		s += 'if({name} >= {a} && {name} <= {b})\n'
+		s += '\tlogL += -log({b} - {a});\n'
+		s += 'else\n'
+		s += '\tlogL += -1E300;\n'
+		return s.format(name=self.name, a=self.a, b=self.b)
+
+	def proposal(self):
+		s = ''
+		s += '{name} += ({b} - {a})*pow(10., 1.5 - 6.*randomU())*randn();\n'
+		s += '{name} = mod({name} - {a}, {b} - {a}) + {a};\n'
+		return s.format(name=self.name, a=self.a, b=self.b)
+
+
+class Normal:
+	def __init__(self, name, mu, sigma):
+		self.name = name
+		self.mu = mu
+		self.sigma = sigma
+
+	def __repr__(self):
+		return '{name} ~ normal({mu}, {sigma});'.format(name=self.name,
+						mu=self.mu, sigma=self.sigma)
+
+	def __str__(self):
+		return self.name
+
+	def from_prior(self):
+		s = ''
+		s += '{name} = {mu} + {sigma}*randn();\n'
+		return s.format(name=self.name,	mu=self.mu, sigma=self.sigma)
+	
+
+	def log_likelihood(self):
+		s = ''
+		s += 'logL += -0.5*log(2.*M_PI) - log({sigma});\n'
+		s += 'logL += -0.5*pow(({name} - {mu})/{sigma}, 2);\n'
+		return s.format(name=self.name,	mu=self.mu, sigma=self.sigma)
+
+	def proposal(self):
+		s = ''
+		s += 'logH -= -0.5*pow(({name} - {mu})/{sigma}, 2);\n'
+		s += '{name} += {sigma}*pow(10., 1.5 - 6.*randomU())*randn();\n'
+		s += 'logH += -0.5*pow(({name} - {mu})/{sigma}, 2);\n'
+		return s.format(name=self.name,	mu=self.mu, sigma=self.sigma)
+
+
+if __name__ == '__main__':
+	theta = Uniform('theta', 0., 1.)
+	x = Uniform('x', 0., theta)
 
 	print("FROMPRIOR")
-	print(n2.from_prior())
+	print(theta.from_prior())
 
 	print("PROPOSAL")
-	print(n2.proposal())
+	print(theta.proposal())
 
+	print("LOGLIKELIHOOD")
+	print(theta.log_likelihood())
+
+	print("\n\n\n")
+
+	print("FROMPRIOR")
+	print(x.from_prior())
+
+	print("PROPOSAL")
+	print(x.proposal())
+
+	print("LOGLIKELIHOOD")
+	print(x.log_likelihood())
 
